056afe8fb93af15c3eb85fcd3b0cbb62c82b4fc7
jinie-singh <74295187+jinie-singh@users.noreply.github.com>
2021-12-24 23:38:45 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# C77-HomeScreen
+# ISS-Tracker-1-Teacher-Ref
+Reference code for C76
---
+++ screens/Home.js
@@ -0,0 +1,56 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { Text, View, TouchableOpacity , StyleSheet, SafeAreaView, Platform} from 'react-native';
+
+export default class HomeScreen extends Component {
+    render() {
+        return (
+            <View style={StyleSheet.container}>
+                <SafeAreaView style = {styles.droidSafeArea} />
+                <View style = {styles.titleBar}>
+                    <Text style = {styles.titleText}>ISS Tracker App</Text>
+                </View>
+                <TouchableOpacity style={styles.routeCard}>
+                    <Text style = {styles.routeText}> ISS Location </Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.routeCard}>
+                    <Text style = {styles.routeText}> Meteors </Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container : {
+        flex : 1
+    },
+    droidSafeArea : {
+        marginTop : Platform.OS === "android" ? StatusBar.currentHeight : 0
+    },
+    titleBar : {
+        flex : 0.15,
+        justifyContent : "center",
+        alignItems: "center"
+    },
+    routeCard : {
+        flex : 0.25,
+        marginLeft : 50,
+        marginRight : 50,
+        marginTop:50,
+        borderRadius : 30,
+        backgroundColor : "white"
+    },
+    routeText : {
+        fontSize : 35,
+        fontWeight : "bold",
+        color : "black",
+        marginTop : 75,
+        paddingLeft : 30
+    },
+    titleText : {
+        fontSize : 40,
+        fontWeight : "bold",
+        color : "black"
+    }
+})
